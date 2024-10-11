9a3b8f6fec19a4b18ebfa3036625542874a57734
yadavsourabhgh <2024.sourabhy@isu.ac.in>
2024-10-11 15:55:56 +0530

    11th october

---
+++ 10th-october.py
@@ -0,0 +1,28 @@
+
+
+# global_var = "Global Var"
+# def fun(n):
+#     local_var = "Local Var" #can be accessed inside the function only
+#     global global_var2 # with this we can declare a global variable inside any function block
+#     global_var2 = 20 # can be accessed abywhere in the code
+
+# print(global_var)
+# # print(local_var) #error
+# print(global_var2)
+
+
+
+
+# a = 20
+# def scopeDemo():
+#     a = 10
+#     print("a =",globals()['a'])
+#     print("a =",a)
+
+# scopeDemo()
+
+
+
+
+# recurssion is the process where function call itself from its own body and that function is called as recurrsive process
+
