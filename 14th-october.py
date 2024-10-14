98d9a9f598389fc40d9cfe85c2c69041495ee693
yadavsourabhgh <2024.sourabhy@isu.ac.in>
2024-10-14 16:02:29 +0530

    14h october

---
+++ 14th-october.py
@@ -0,0 +1,11 @@
+# set - unorder - dont have indexing 
+# list - tupple - have index
+# list - tubble - allowed to enter diff type of element 
+# in set we cant reapeat the element
+# [] - list 
+# () - tupple
+# {} - dict
+# set() - set
+
+s1=set()
+print(type(s1))
