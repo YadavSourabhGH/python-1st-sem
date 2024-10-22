2c28468463ed712463ddd2613b3442f38e2850b4
yadavsourabhgh <2024.sourabhy@isu.ac.in>
2024-10-22 17:15:20 +0530

    22nd october

---
+++ 21-october.py
@@ -0,0 +1,39 @@
+# file i/o 
+
+# syntex 
+# file_object = open("file_path","mode")
+# file_object.close()
+
+
+# f = open("/Users/sourabhyadav/Desktop/assignment/folder-2/test.txt","w")
+# data = "Itm skills university"
+
+# f.write(data)
+
+
+# f = open("/Users/sourabhyadav/Desktop/assignment/folder-2/test.txt","r")
+
+# data=f.read()
+# print(data)
+
+# f = open("/Users/sourabhyadav/Desktop/assignment/folder-2/test.txt","a")
+# data = "Itm skills university"
+
+# f.write(data)
+# f.write("\nHELLO")
+# f.close()
+
+
+# f=open("/Users/sourabhyadav/Desktop/assignment/folder-2/test.txt",'a') #used relative path
+# data="\nHello"
+# f.write(data)
+# f=open("/Users/sourabhyadav/Desktop/assignment/folder-2/test.txt",'r') #used relative path
+# data=f.readlines()
+# print("Data : ",data)
+# print("File Contentes : ", data)
+# c=1
+# for line in data:
+#     print(c,line)
+#     c+=1
+# f.close()
+
