422e1712b188f68f7ce10172e61cee9a960ae373
yadavsourabhgh <2024.sourabhy@isu.ac.in>
2024-11-07 22:09:11 +0530

    7th-november

---
+++ 7th-november.py
@@ -0,0 +1,32 @@
+# lambda function (anonoymous function) in python
+
+# def m(x, y):
+#     while x<=y: 
+#         yield x 
+#         x+=1
+
+# g = m(5, 10)
+# for y in g:
+#     print (y)
+
+
+
+# fibonacci sequence
+
+# def fibonacci_generator(no_of_term):
+#     i=0
+#     n1=0
+#     n2=1
+#     sum=1
+ 
+#     while(i<=no_of_term):
+
+#         yield sum
+#         n1=n2
+#         n2=sum
+#         sum=n1+n2
+#         i+=1
+
+# fs=fibonacci_generator(5)
+# for p in fs:
+#     print(p)
---
+++ Assignment/4th-november-part-B-assignment.py
@@ -0,0 +1,8 @@
+# # Write a program to exhibit these concepts:
+# #            i. try
+# #            ii. except
+# #            iii. finally
+
+# try:
+#     print("heloo")
+# except:
