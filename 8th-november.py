046de6804ffc940a121265e3c6e88cf051a44272
yadavsourabhgh <2024.sourabhy@isu.ac.in>
2024-11-10 12:18:55 +0530

    10th november commit

--- 8th-november.py
+++ 8th-november.py
@@ -1,5 +1,6 @@
 
 from tkinter import *
+from tkinter import messagebox
 '''
 import tkinter
 
@@ -52,9 +53,9 @@ C1.pack()
 C2.pack()
 
 top.mainloop()
-'''
 
-'''
+
+
 from tkinter import messagebox
 
 root = Tk()
@@ -106,8 +107,45 @@ B1= Button(root, text = "Answer", command = calculate)
 B1.pack()
 
 root.mainloop()
+
+
+
+
+from tkinter import *
+
+def sel():
+    selection = "You selected the option "+str(var.get())
+    label.config(text=selection)
+
+root = Tk()
+
+var=IntVar()
+
+R1=Radiobutton(root, text="Option 1", variable=var, value=1, command=sel)
+
+R1.pack()
+
+R2=Radiobutton(root, text="Option 2", variable=var, value=2, command=sel)
+
+R2.pack()
+
+R3=Radiobutton(root, text="Option 3", variable=var, value=3, command=sel)
+
+R3.pack()
+
+label=Label(root)
+
+label.pack()
+
+root.mainloop()
+
+
+
+
+
+root = Tk()
+label = Label(root,text="hello")
+label.config(font=("Helvetica", 36, "bold"))
+label.pack()
+root.mainloop()
 '''
-
-
-
-
